75afe51330abcdf5dda862255812df2de15871d6
Yang ChenMing <chenming.yang@sc.com>
2018-03-07 11:37:03 +0800

    add new gradle sample

---
+++ JsonParser/run_me.bat
@@ -0,0 +1,1 @@
+gradle -Pservice=somedata restJson
---
+++ SpringBoot/run_me.bat
@@ -0,0 +1,7 @@
+@echo first, run gradle build
+@echo secondly, run java -jar build/libs/gs-spring-boot-0.1.0.jar (then observe the logs printed)
+@echo thirdly, visit http://localhost:8080/ or do curl localhost:8080
+@echo lastly, visit localhost:8080/health, localhost:8080/info
+@echo press Ctrl+C to stop the server
+@echo Note that, the steps may change as spring boot envolve over time; 
+@echo the version of SpringBoot is certainly outdated when you see this message
